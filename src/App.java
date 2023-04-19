7f10e634fdb1415db642d81e1a4d517221e21885
rayhankinan <rayhankinan@gmail.com>
2023-04-19 20:43:29 +0700

    Finalize email parsing

--- src/App.java
+++ src/App.java
@@ -1,23 +1,8 @@
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.security.PrivateKey;
-import java.security.PublicKey;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        BigInteger a = new BigInteger("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC", 16);
-        BigInteger b = new BigInteger("5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B", 16);
-        BigInteger p = new BigInteger("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF", 16);
-
-        EllipticalCurve ellipticalCurve = new EllipticalCurve(a, b, p);
-        Point basePoint = new Point(
-                new BigInteger("6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296", 16),
-                new BigInteger("4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5", 16));
-        BigInteger n = new BigInteger("FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551", 16);
-
-        byte[] message = "Hello World!".getBytes();
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256"); // INI DIGANTI DENGAN ALGORITMA GAGAS
-        byte[] hash = messageDigest.digest(message);
+        String message = "Hello World!";
 
         EllipticalCurveKeyStore ellipticalCurveKeyStore = new EllipticalCurveKeyStore("key/example.p12",
                 "password");
@@ -25,18 +10,21 @@ public class App {
 
         PrivateKey readPrivateKey = ellipticalCurveKeyStore.read();
 
-        // PrivateKey initialPrivateKey =
-        // EllipticalCurveKey.generatePrivateKey(ellipticalCurve, basePoint, n);
+        // PrivateKey initialPrivateKey = EmailParser.generatePrivateKey();
         // ellipticalCurveKeyStore.save(initialPrivateKey);
         // ellipticalCurveKeyStore.store();
 
-        PublicKey publicKey = EllipticalCurveKey.generatePublicKey(ellipticalCurve, basePoint, n, readPrivateKey);
+        String wholeMessage = EmailParser.signMessage(readPrivateKey, message);
 
-        DigitalSigning digitalSigning = new DigitalSigning(ellipticalCurve, basePoint, n, readPrivateKey);
-        byte[] sign = digitalSigning.getSigning(hash);
+        String[] firstPartition = wholeMessage.split("\n---BEGIN SIGNATURE---\n");
+        String receivedMessage = firstPartition[0];
 
-        DigitalVerifying digitalVerifying = new DigitalVerifying(ellipticalCurve, basePoint, n, publicKey);
-        boolean result = digitalVerifying.verifySigning(hash, sign);
+        String[] secondPartition = firstPartition[1].split("\n---END SIGNATURE---\n---BEGIN PUBLIC KEY---\n");
+        String sign = secondPartition[0];
+        String publicKey = secondPartition[1].replaceAll("\n---END PUBLIC KEY---\n", "");
+
+        boolean result = EmailParser.verifyMessage(receivedMessage, sign, publicKey);
+
         System.out.println(result);
     }
 }
--- src/EmailParser.java
+++ src/EmailParser.java
@@ -1,7 +1,21 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Base64;
 
 public class EmailParser {
+    public static BigInteger a = new BigInteger("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFC", 16);
+    public static BigInteger b = new BigInteger("5AC635D8AA3A93E7B3EBBD55769886BC651D06B0CC53B0F63BCE3C3E27D2604B", 16);
+    public static BigInteger p = new BigInteger("FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF", 16);
+    public static EllipticalCurve ellipticalCurve = new EllipticalCurve(a, b, p);
+    public static Point basePoint = new Point(
+            new BigInteger("6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296", 16),
+            new BigInteger("4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5", 16));
+    public static BigInteger n = new BigInteger("FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551", 16);
+
     public static String exportPublicKey(PublicKey publicKey) {
         byte[] encodedPublicKey = publicKey.getEncoded();
 
@@ -34,8 +48,49 @@ public class EmailParser {
     }
 
     public static byte[] importSignature(String rawSignature) {
-        byte[] decodedSignature = Base64.getDecoder().decode(rawSignature);
+        return Base64.getDecoder().decode(rawSignature);
+    }
 
-        return decodedSignature;
+    public static PrivateKey generatePrivateKey() {
+        return EllipticalCurveKey.generatePrivateKey(EmailParser.ellipticalCurve, EmailParser.basePoint, EmailParser.n);
+    }
+
+    public static PublicKey generatePublicKey(PrivateKey privateKey) throws IOException {
+        return EllipticalCurveKey.generatePublicKey(EmailParser.ellipticalCurve, EmailParser.basePoint, EmailParser.n,
+                privateKey);
+    }
+
+    public static String signMessage(PrivateKey privateKey, String message)
+            throws NoSuchAlgorithmException, IOException {
+        byte[] rawMessage = message.getBytes();
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256"); // INI DIGANTI DENGAN ALGORITMA GAGAS
+        byte[] hash = messageDigest.digest(rawMessage);
+
+        DigitalSigning digitalSigning = new DigitalSigning(EmailParser.ellipticalCurve, EmailParser.basePoint,
+                EmailParser.n, privateKey);
+        byte[] rawSign = digitalSigning.getSigning(hash);
+        String sign = EmailParser.exportSignature(rawSign);
+
+        PublicKey rawPublicKey = EllipticalCurveKey.generatePublicKey(ellipticalCurve, basePoint, n, privateKey);
+        String publicKey = EmailParser.exportPublicKey(rawPublicKey);
+
+        return message + "\n"
+                + "---BEGIN SIGNATURE---\n" + sign + "\n---END SIGNATURE---\n"
+                + "---BEGIN PUBLIC KEY---\n" + publicKey + "\n---END PUBLIC KEY---\n";
+    }
+
+    public static boolean verifyMessage(String message, String sign, String publicKey)
+            throws NoSuchAlgorithmException, IOException {
+        byte[] rawMessage = message.getBytes();
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256"); // INI DIGANTI DENGAN ALGORITMA GAGAS
+        byte[] hash = messageDigest.digest(rawMessage);
+
+        PublicKey rawPublicKey = EmailParser.importPublicKey(publicKey);
+        byte[] rawSign = EmailParser.importSignature(sign);
+
+        DigitalVerifying digitalVerifying = new DigitalVerifying(EmailParser.ellipticalCurve, EmailParser.basePoint,
+                EmailParser.n, rawPublicKey);
+
+        return digitalVerifying.verifySigning(hash, rawSign);
     }
 }
